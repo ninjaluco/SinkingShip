58226f494968145a7849c23abcaf859228e89c9e
rebeckanylander <rebecka.nylander@hotmail.com>
2017-10-06 16:42:16 +0200

    Efter redovisning

--- SinkingShip/js/script.js
+++ SinkingShip/js/script.js
@@ -1,18 +1,37 @@
-﻿function StartGame() {
+﻿var tableSize = 5;
+var grid = [];
+var shipID = Random(tableSize);
 
-    var tableSize = 3;
-    var grid = [];
-
+function StartGame() {
+    
     for (var i = 0; i < tableSize * tableSize; i++) {
         $("#gamePlan").append("<div id=\"Square_" + i + "\" class=\"Square\" onclick=\"SquareClicked(" + i + ")\"></div>");
 
         if ((i + 1) % tableSize == 0)
             $("#gamePlan").append("<br />");
     }
-    //test
+
+
+    
+    alert(shipID);
 
 }
 
 function SquareClicked(i) {
-    $("#Square_" + i).addClass("SquareMiss");
+    if ("Square_" + i == shipID) {
+        $("#Square_" + i).addClass("SquareBoom");
+    }
+
+    else {
+        $("#Square_" + i).addClass("SquareMiss");
+    }
 }
+
+function Random(tableSize) {
+    var min = Math.ceil(0);
+    var max = Math.floor((tableSize * tableSize) - 1);
+    var shipID = "Square_" + (Math.floor(Math.random() * (max - min)) + min);
+    alert(shipID);
+    return shipID;
+}
+    
